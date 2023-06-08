8bd053347d324643416a187aef29c0c5ed8b4d50
Chandrakant Koneti <konetichandrakant@gmail.com>
2023-06-08 11:19:59 +0530

    added attendance routes

--- routes/attendance.js
+++ routes/attendance.js
@@ -4,16 +4,28 @@ const router = express.Router();
 const { Course } = require('../model/Course')
 const { courseDetails } = require('../details');
 
-router.post('/student-attendance', (req, res) => {
+router.post('/student-attendance', async (req, res) => {
   const { studentId, courseId, dateAndTime } = req.body;
-  const courseData = courseDetails(courseId);
-  
+  const courseData = { ...courseDetails(courseId) };
+  if (!courseData)
+    return res.send({ valid: false });
+  let changed = false;
+  for (let i = 0; i < courseData['students'].length; i++) {
+    if (courseData['students'][i]['studentId'] === studentId) {
+      courseData['students'][i]['classesAttended'].push(dateAndTime);
+      changed = true;
+      break;
+    }
+  }
+  if (changed)
+    await Course.findOneAndUpdate({ courseId: courseId }, courseData);
 })
 
-router.post('/teacher-attendance', (req, res) => {
-  const { studentId, courseId, dateAndTime } = req.body;
-})
-
-router.post('/teacher-attendance', (req, res) => {
-
+router.post('/teacher-attendance', async (req, res) => {
+  const { teacherId, courseId, dateAndTime } = req.body;
+  const courseData = { ...courseDetails(courseId) };
+  if (!courseData || courseData['teacherId'] !== teacherId)
+    return res.send({ valid: false });
+  courseData['classesTaken'].push(dateAndTime);
+  await Course.findOneAndUpdate({ courseId: courseId }, courseData)
 })
